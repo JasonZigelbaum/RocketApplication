e5f1e7108a330d99a43c3fbc113175601a9d5fe2
Jason <jason@Jasons-MacBook-Pro.local>
2011-05-29 06:01:03 -0400

    Initial Commit

---
+++ TinyRockets/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  TinyRockets
+//
+//  Created by Jason on 5/29/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ TinyRockets/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  TinyRockets
+//
+//  Created by Jason on 5/29/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
